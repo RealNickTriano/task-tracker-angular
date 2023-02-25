b9e960755c81907db195154411eb7205680fce47
Nicholas Triano <nicholastriano1999@gmail.com>
2023-02-25 16:24:38 -0500

    filter projects by status, no ux

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Project } from 'src/app/common/models/Project';
 import { ProjectService } from 'src/app/common/services/project.service';
 
+interface Filter {
+  name: string,
+  key: string
+}
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -10,6 +15,14 @@ import { ProjectService } from 'src/app/common/services/project.service';
 export class ProjectsComponent implements OnInit {
 
   projects: Project[] = [];
+  selectedProjects: Project[] = [];
+
+  filters: Filter[] = [
+    {name: "All Projects", key: ""},
+    {name: "In Progress", key: "In Progress"},
+    {name: "Not Started", key: "Not Started"},
+    {name: "Completed", key: "Completed"},
+  ]
 
   constructor(private projectService: ProjectService) {}
 
@@ -19,7 +32,19 @@ export class ProjectsComponent implements OnInit {
 
   fetchProjects() {
     this.projectService.getAllProjects()
-      .subscribe((projects: Project[]) => this.projects = projects);
+      .subscribe((projects: Project[]) => {
+        this.projects = projects;
+        this.selectedProjects = projects;
+      });
+  }
+
+  filterProjectsByStatus(key: string) {
+    if (key === "") {
+      this.selectedProjects = this.projects;
+    } else {
+      this.selectedProjects = this.projects.filter(
+        item => item.status === key);
+    }
   }
 
 }
