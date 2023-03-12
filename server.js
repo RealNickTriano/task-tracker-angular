030ec989e67dc55b9b92f392e5fc154e3b49071e
Nicholas Triano <nicholastriano1999@gmail.com>
2023-03-12 11:32:57 -0400

    config changes

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/task-tracker2'));
+app.use(express.static('./dist/task-tracker2'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/task-tracker2/index.html'));});
+res.sendFile('./dist/task-tracker2/index.html')});
 app.listen(process.env.PORT || 8080);
